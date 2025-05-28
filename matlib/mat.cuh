f75226cbeeb58031acad55f1f6e907f56b912305
roshan <roshan@aprabhakar.com>
2025-05-28 15:45:04 -0700

    added matrix class

--- matlib/mat.cuh
+++ matlib/mat.cuh
@@ -5,51 +5,65 @@
 
 typedef float nn_real;
 
+template <typename OrderPolicy>
 class Matrix {
 	protected:
-		int rows_, cols_, size_;
+		int rows_, cols_, size_, stride_;
 		nn_real *d_data_;
 
 	public:
-		Matrix(int rows, int cols)
-			: rows_(rows), cols_(cols), size_(rows*cols), d_data_(nullptr) { }
-		Matrix(int row, int cols, nn_real *data) 
-			: rows_(rows), cols_(cols), size_(rows*cols), d_data_(data) { }
+		Matrix(int rows, int cols, nn_real *data)
+			: rows_(rows), cols_(cols), size_(rows*cols), stride_(stride), d_data_(data)  { 
+				stride_ = OrderPolicy::stride(rows, cols);
+		}
 
-		~Matrix() { if (d_data_) cudaFree(d_data_); }
+		Matrix(int rows, int cols, nn_real *data, int stride)
+			: rows_(rows), cols_(cols), size_(rows*cols), stride_(stride), d_data_(data)  { }
+		// Implicit destructor OK, we do not want to be manually deleting the gpu memory.
 
-		__device__ int size(void) const { return rows_ * cols_; }
-		__device__ int rows(void) const { return rows_; }
-		__device__ int cols(void) const { return cols_; }
+		__host__ __device__ int size(void) const { return rows_ * cols_; }
+		__host__ __device__ int rows(void) const { return rows_; }
+		__host__ __device__ int cols(void) const { return cols_; }
+		__host__ __device__ int stride(void) const { return stride_; }
+
 		__device__ nn_real *data() const { return d_data_; }
 
-		virtual __device__ nn_real &operator()(int row, int col) = 0;
-};
+		__device__ nn_real &operator()(int row, int col) {
+			return d_data_[OrderPolicy::index(row, col, this->rows(), this->cols(), this->stride())];
+		}
 
-class DenseMatrix : public Matrix {
-	public:
-		DenseMatrix(int rows, int cols): Matrix(rows, cols) {
-			nn_real *data = nullptr;
-			CUDA_CHECK(cudaMalloc((void **)&data, this->size() * sizeof(nn_real)));
+		__device__ Matrix<OrderPolicy> getSubMatrix(int row, int col, int numRows, int numCols) {
+			return Matrix<OrderPolicy>(
+					numRows, 
+					numCols, 
+					this->d_data + OrderPolicy::index(row, col, this->stride()),
+					this->stride()
+				);
+		}
 
-			this->d_data_ = std::shared_ptr<nn_real>(data, [](nn_real *ptr) {
-					if (ptr) { cudaFree(ptr); }
-			});
+		__host__ nn_real *getHostCopy() {
+			nn_real *ptr = malloc(this->size() * sizeof(nn_real));
+			cudaMemcpy(ptr, this->d_data_, this->size() * sizeof(nn_real), cudaMemcpyDeviceToHost);
+			return ptr;
 		}
 };
 
-class RowMajorMatrix : public DenseMatrix {
+class RowMajor {
 	public:
-		__device__ nn_real &operator()(int row, int col) {
-			return this->d_data_[row * this->cols() + col];
-		};
+		static __host__ __device__ int index(int row, int col, int stride) { return row * stride + col; }
+		static __host__ __device__ int stride(int rows, int cols) { return cols; }
 };
 
-class ColMajorMatrix : public DenseMatrix {
+class ColMajor {
 	public:
-		__device__ nn_real &operator()(int row, int col) {
-			return this->d_data_[col * this->rows() + row];
-		}
+		static __host__ __device__ int index(int row, int col, int stride) { return col * stride + row; }
+		static __host__ __device__ int stride(int rows, int cols) { return rows; }
 };
+
+template <typename OutPolicy, typename APolicy, typename BPolicy, typename BiasPolicy>
+void mul(Matrix<OutPolicy>& out, 
+         const Matrix<APolicy>& A,
+         const Matrix<BPolicy>& B,
+         const Matrix<BiasPolicy>& bias);
 
 #endif // LLMC_MAT_LIB
--- train_gpt2_fp32.cu
+++ train_gpt2_fp32.cu
@@ -33,6 +33,8 @@
 // defines: dataloader_init, dataloader_reset, dataloader_next_batch, dataloader_free
 #include "llmc/dataloader.h"
 
+#include "matlib/mat.cuh"
+
 // ----------------------------------------------------------------------------
 // CUDA utils
 
@@ -624,81 +626,6 @@ __device__ void st_vec(float* address, float4 val) {
   *reinterpret_cast<float4*>(address) = val;
 }
 
-__global__ void __launch_bounds__(16*16, 2) matmul_forward_kernel4(float* out,
-    const float* inp, const float* weight, const float* bias,
-    int C, int OC) {
-
-	// blockIdx.y
-	// 			The index of the output tile along the cols dimension
-	// blockIdx.x
-	// 			The index of the output tile along the rows dimension
-
-  // out is (B,T,OC). OC is short for "output channels", e.g. OC = 4 * C
-  // inp is (B,T,C), weight is (OC, C), bias is (OC)
-  // each thread handles 8x8 elements; each block 128 by 128 elements.
-  int oc = 8*(blockIdx.y * blockDim.y + threadIdx.y);
-
-  // buffers to cache chunks of the input matrices
-  __shared__ float lhs_s[128][32];
-  __shared__ float rhs_s[128][32];
-
-  // adjust our pointers for the current block
-  inp += 128 * blockIdx.x * C;
-  weight += 128 * blockIdx.y * C;
-  out += 128 * blockIdx.x * OC + 128 * blockIdx.y;
-
-  float vals[8][8] = {};
-  if(bias != NULL) {
-    for (int i = 0; i < 8; i++) {
-      for (int j = 0; j < 8; j += 4) {
-				st_vec(&vals[i][j], ld_vec(bias + oc + j));
-      }
-    }
-  }
-
-  int si_start = 4*(16 * threadIdx.y + threadIdx.x);
-  for (int so = 0; so < C; so += 32) {
-    __syncthreads();
-    int xmod8 = threadIdx.x % 8;
-    int xby8 = threadIdx.x / 8;
-    int xo = 4 * xmod8;
-    for(int y = 2 * threadIdx.y + xby8; y < 128; y += 32) {
-      st_vec(&lhs_s[y][xo], ld_vec(inp + y * C + so + xo));
-      st_vec(&rhs_s[y][xo], ld_vec(weight + y * C + so + xo));
-    }
-    __syncthreads();
-
-    for (int si = si_start; si < si_start + 32; si += 4) {
-      float4 rhs[8];
-      for (int u = 0; u < 8; ++u) {
-        rhs[u] = ld_vec(&rhs_s[u + 8 * threadIdx.y][si % 32]);
-      }
-
-      for (int ii = 0; ii < 8; ++ii) {
-        float4 lhs = ld_vec(&lhs_s[ii + 8 * threadIdx.x][si % 32]);
-        for (int ji = 0; ji < 8; ++ji) {
-          vals[ii][ji] += lhs.x * rhs[ji].x;
-          vals[ii][ji] += lhs.y * rhs[ji].y;
-          vals[ii][ji] += lhs.z * rhs[ji].z;
-          vals[ii][ji] += lhs.w * rhs[ji].w;
-        }
-      }
-    }
-  }
-
-  for (int i = 0; i < 8; ++i) {
-    for (int j = 0; j < 8; j += 4) {
-      float4 result;
-      result.x = vals[i][j + 0];
-      result.y = vals[i][j + 1];
-      result.z = vals[i][j + 2];
-      result.w = vals[i][j + 3];
-      st_vec(out + (8*threadIdx.x+i) * OC + 8*threadIdx.y + j, result);
-    }
-  }
-}
-
-
 // ----------------------------------------------------------------------------
 // kernel launchers
 
@@ -734,7 +661,7 @@ void layernorm_forward(float* out, float* mean, float* rstd,
 }
 
 // kernel 1 is the most naive matmul kernel
-void matmul_forward(float* out,
+void matmul_forward_correct(float* out,
     const float* inp, const float* weight, const float* bias,
     int B, int T, int C, int OC) {
   // out is (B,T,OC). OC is short for "output channels", e.g. OC = 4 * C
@@ -746,6 +673,36 @@ void matmul_forward(float* out,
   matmul_forward_kernel4<<<gridDim, blockDim>>>(out, inp, weight, bias, C, OC);
   cudaCheck(cudaGetLastError());
 }
+
+void matmul_forward(
+		float *out, float const *x, float const *param, float const *bias,
+		int B, int T, int C, int OC) {
+
+  // out is (B,T,OC). OC is short for "output channels", e.g. OC = 4 * C
+  // inp is (B,T,C), weight is (OC, C), bias is (OC)
+
+	Matrix<RowMajor> inp(B*T, C, x);
+	Matrix<ColMajor> param(C, OC, param);
+	Matrix<RowMajor> bias(1, OC, bias);
+	Matrix<RowMajor> out(B*T, OC, out)
+
+	Matrix::mul(out, inp, param, bias);
+
+	nn_real *bias_ptr = bias.getHostCopy();
+	nn_real *out_ptr = out.getHostCopy();
+
+	printf("-------------------");
+	for (int i = 0; i < 10; ++i) {
+		printf("[bias: %f, out: %f] ", bias_ptr[i], out_ptr[i]);
+	}
+	printf("-------------------");
+
+	// Copy bias to host
+	// Copy row i from output to host
+	// Make sure they are the same
+
+}
+
 
 void attention_forward(float* out, float* qkvr, float* att,
     float* inp,
